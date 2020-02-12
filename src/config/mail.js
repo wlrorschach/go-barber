cccc5d89179ad257ff15849a2a13d499d1eabe83
Wiliam <wlrorschach@gmail.com>
2020-02-12 08:55:12 -0300

    Incluindo arqui de vairaveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'ee336fe29ae0b1',
-    pass: '3be37dec963534',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://e94547e9aaf943eebfb5dcb259f1a660@sentry.io/2444756',
+  dsn: process.env.SENTRY_DSN,
 };
